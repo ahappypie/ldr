931e351c196f1e54688844028cdbe9b2bfef9d19
Brian Bagdasarian <bbagdasarian.scs@gmail.com>
2017-01-13 10:45:04 -1000

    Initial commit

---
+++ ldr.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Brian on 12/22/16.
+ */
+const Twitter = require('twitter');
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+client.stream('statuses/filter', {track: 'javascript'},  (stream) => {
+    console.log('got here');
+    stream.on('data', (tweet) => {
+        console.log(tweet);
+    });
+
+    stream.on('error', (error) => {
+        console.log(error);
+    });
+});
